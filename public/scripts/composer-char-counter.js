2f70964904738689379a8439e3ab4cd3811e8c68
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-30 15:49:36 -0400

    implemented character counter and it turns red when over 140 chars

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function () {
+  //  event handler for the textarea
+  $("#tweet-text").on("input", function (e) {
+    // update the character counter
+
+    const remainingChars = 140 - $(this).val().length;
+    // $('.counter').text(remainingChars);
+    const formFind = $(this).closest("form");
+    const countTarget = formFind.find("output");
+    countTarget.text(remainingChars);
+
+    if (remainingChars < 0) {
+      countTarget.css("color", "red")
+    } else {
+      countTarget.css("color", "")
+    }
+
+  });
+
+});
